9d66d3420745401fdb0a172faba588699045d0c5
Nilos Makino <nmakino@bu.edu>
2020-09-22 17:23:03 -0400

    Finished library tasks, passed all tests

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,20 @@
+import csv
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path, newline='') as csvfile: 
+        reader = csv.reader(csvfile, delimiter=' ', quotechar='"')
+        
+        matrix = []
+        for row in reader: 
+            newrow = []
+            for x in row[0].split(','):
+                if x.isnumeric():
+                    newrow += [int(x)]
+                else:
+                    newrow += [x]
+            matrix += [newrow]
+        return matrix
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,46 @@
+import math
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        return ValueError("lengths must not be zero")
+    if (len(x) != len(y)):
+        return ValueError("lengths must be equal") 
+    
+    sum = 0
+    for i in range(len(x)):
+        sum += ((x[i] - y[i]) ** 2)
+    return (sum ** .5)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        return ValueError("lengths must not be zero")
+    if (len(x) != len(y)):
+        return ValueError("lengths must be equal") 
+    sum = 0
+    for i in range(len(x)):
+        sum += abs(x[i] - y[i])
+    return sum
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        return ValueError("lengths must not be zero")
+    if (len(x) != len(y)):
+        return ValueError("lengths must be equal") 
+    return 1 - (len(set(x) & set(y))/len(set(x) | set(y)))
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if (x == [] or y == []):
+        return ValueError("lengths must not be zero")
+    if (len(x) != len(y)):
+        return ValueError("lengths must be equal") 
+    dot_prod = 0
+    xsum = 0
+    ysum = 0
+    for i in range(len(x)):
+        dot_prod += (x[i]*y[i])
+        xsum += x[i]**2
+        ysum += y[i]**2
+    return dot_prod/((xsum*ysum)**.5)
+    
 
 # Feel free to add more
